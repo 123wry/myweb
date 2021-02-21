d0ae2aed4f5742fe4584a8999c2e992fac7fb2e1
unknown <740820470@qq.com>
2021-02-21 23:53:33 +0800

    20210221-24

--- app/index/controller/Home.php
+++ app/index/controller/Home.php
@@ -102,20 +102,20 @@ class Home extends Base
             "fileList"=>$files
         ]);
         $article->save();dump($tags);
-//        foreach ($tags as $t) {
-//           $mtag->data([
-//                "tag_id" =>$t,
-//                "article_id"=>$article_id
-//            ]);
-//            $result = $mtag->save();
-//        }
-//        if(!empty($result)){
-//            $ret['errno'] = 400;
-//            $ret['errmsg'] = '提交失败';
-//        } else {
-//            $ret['errno'] = 200;
-//            $ret['errmsg'] = '提交成功';
-//        }
-//        $this->ajaxReturn($ret);
+        foreach ($tags as $t) {
+           $mtag->data([
+                "tag_id" =>$t['tag_id'],
+                "article_id"=>$article_id
+            ]);
+            $result = $mtag->save();
+        }
+        if(!empty($result)){
+            $ret['errno'] = 400;
+            $ret['errmsg'] = '提交失败';
+        } else {
+            $ret['errno'] = 200;
+            $ret['errmsg'] = '提交成功';
+        }
+        $this->ajaxReturn($ret);
     }
 }
